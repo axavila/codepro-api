22996295233106d3f1e6129c2bddf5151538f361
axavila <ax.avila@hotmail.com>
2022-03-06 20:23:06 -0600

    adding knex npm

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+
+
+
+
+
+
+
+
+const app = express();
+
+
+
+app.use(bodyParser.json());
